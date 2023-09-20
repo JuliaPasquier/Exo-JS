cc52afdb5acb9a44fbec2f4b4b271a3d2cbb13c9
Julia <pasquierjulia5@gmail.com>
2023-09-20 09:02:01 +0200

    Pour ma ccheeerrriiiiieeeeeee

---
+++ script.js
@@ -0,0 +1,81 @@
+let num01 = prompt("enter first num");
+let num02 = prompt ("enter second num");
+let SOMME = parseFloat(num01) + parseFloat(num02);
+
+alert (` resultat:  ${SOMME}`);
+
+
+
+// let num01 = prompt("enter first num");
+// let num02 = prompt ("enter second num");
+// let SOMME = parseFloat(num01) + parseFloat(num02);
+
+// alert (` resultat:  ${SOMME}`);
+
+
+
+// let R =prompt('rayon?');
+// const pi = 3.14;
+// let surface = (R*2)*pi;  
+
+// alert(`Surface totale du cercke: ${surface}`);
+
+
+// let myNumber = "52";
+// console.log(typeof myNumber);
+
+
+
+// let num= 54;
+// let numS='bonjour';
+// let resultat=num + numS;
+
+// console.log(resultat);
+
+
+// let prixHT = prompt("prix sans TVA" );
+// let prixTva = prixHT*0.21;
+// let prixTTC = parseFloat(prixHT)+(prixTva);
+
+// alert(`prix TVA : ${prixTTC}`);
+
+
+
+// const name = prompt("What is your name?");
+// const age = prompt("how old are you?");
+// const city = prompt("where do you live?");
+
+// alert(`Welcome ! ${name}  ${age} ${city}`);
+
+
+// const word = "skateboard"; 
+
+// const facialHair = word.slice(5).replace('o', 'e');
+// console.log(facialHair);
+
+
+// let name = "Dumbledore";
+// console.log(name.slice(4,5));
+// console.log(str[str.lenght -1]);
+
+
+
+// const message = "    TASTE THE RAINBOW!  "; 
+
+// const whisper = message.toLowerCase().trim();
+// console.log(whisper);
+
+
+
+// var a = "3";
+// var b = "8";
+
+// /***********Do not change the code above 👆*******/
+// //Write your code here :
+// var c = b;
+// var b = a;
+// var a = c;
+// /***********Do not change the code below 👇*******/
+
+// console.log("a is " + a);
+// console.log("b is " + b);
